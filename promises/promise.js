8e77d3523e46e4de36d75ff6703d6aec76c7601b
Matt Bidewell <mattbidewell@Matts-MacBook-Pro.local>
2022-06-18 19:05:59 +0100

    tidy up

--- promises/promise.js
+++ promises/promise.js
@@ -1,16 +1,28 @@
-import { readFile } from "node:fs/promises";
+/**
+ * Handling Promises.
+ *
+ * The following code represents examples of how to
+ * handle promises.
+ */
 
-function readFilePromise() {
-  readFile("../data.txt", "utf-8")
+import { readFile } from "fs/promises"; // use the promise version of fs.
+
+// Without using async await, to handle a promise you
+// need to chain.then(), .catch() or.finally()
+function main() {
+  readFile("../data.txt", "utf8")
     .then((data) => {
-      console.log("File loaded!");
+      console.log("file loaded");
+      console.log(data);
     })
     .catch((err) => {
-      console.log("opps error!");
-      console.log(err);
-    });
+      console.log("opps error");
+      console.error(err);
+    })
 }
-console.log("**** PROMISE ****");
-console.log("whats first...");
-readFilePromise();
-console.log("or this?");
+
+// what do you think will execute first?
+console.log("**** PROMISE **** ");
+console.log("line 1");
+main();
+console("line 2")
